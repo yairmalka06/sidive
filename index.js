26b2be03d5db12a2b7a04cf0d03fda1bfb2920cf
yairmalka06 <34348548+yairmalka06@users.noreply.github.com>
2018-08-11 19:02:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,15 +30,6 @@ var ownerID = "279353707567644693"
 
 let xp = require("./xp.json");
 
-fs.readdir("./commands/", (err, files) => {
-
-if(err) console.log(err);
-let jsfile = files.filter(f => f.split(".").pop() === "js");
-if(jsfile.length <= 0){
-  console.log("Couldn't find commands.");
-  return;
-}
-
 jsfile.forEach((f, i) =>{
   let props = require(`./commands/${f}`);
   console.log(`${f} loaded!`);
